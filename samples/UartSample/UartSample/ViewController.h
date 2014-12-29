50b8de5614ec7a3c279c789e827f84a95667aa3d
Takashi Sagisaka <sagisaka@ux-xu.com>
2014-12-30 01:46:31 +0900

    detailed UARTSample

--- samples/UartSample/UartSample/ViewController.h
+++ samples/UartSample/UartSample/ViewController.h
@@ -11,8 +11,17 @@
 
 
 @property (weak, nonatomic) IBOutlet UILabel *statusMessage;
+@property (weak, nonatomic) IBOutlet UITextView *uartRecvText;
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *MSDButtons;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *LSDButtons;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *baudButtons;
+@property (weak, nonatomic) IBOutlet UILabel *sendByte;
 
 - (IBAction)find:(id)sender;
 - (IBAction)send:(id)sender;
+- (IBAction)clearRx:(id)sender;
+- (IBAction)changedSendByte:(id)sender;
+- (IBAction)changedBaud:(id)sender;
 
 @end
--- samples/UartSample/UartSample/ViewController.m
+++ samples/UartSample/UartSample/ViewController.m
@@ -14,6 +14,20 @@
 
 @implementation ViewController
 
+uint8_t send_byte;
+
+
+- (void)pushLog:(NSString*)record
+{
+    NSDate *date = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"HH:mm:ss";
+    NSString *date_string = [dateFormatter stringFromDate:date];
+    @synchronized (self) {
+        self.uartRecvText.text = [NSString stringWithFormat:@"%@%@ %@\n", self.uartRecvText.text, date_string, record];
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -23,7 +37,46 @@
     
     [Konashi addObserver:self selector:@selector(connected) name:KonashiEventConnectedNotification];
     [Konashi addObserver:self selector:@selector(ready) name:KonashiEventReadyToUseNotification];
-    [Konashi addObserver:self selector:@selector(recvUartRx) name:KonashiEventUartRxCompleteNotification];
+    [Konashi addObserver:self selector:@selector(disconnected) name:KonashiEventDisconnectedNotification];
+    
+    self.uartRecvText.text = @"";
+    
+    // UART系のイベントハンドラ
+    [Konashi shared].uartRxCompleteHandler = ^(NSData *data) {
+        NSLog(@"uart RX complete:%@", [data description]);
+        [self pushLog:[NSString stringWithFormat:@"recv %@", [data description]]];
+    };
+
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)setSendData:(uint8_t)data
+{
+    @synchronized (self) {
+        send_byte = data;
+        self.sendByte.text = [NSString stringWithFormat:@"0x%02X", send_byte];
+        self.LSDButtons.selectedSegmentIndex = send_byte % 16;
+        self.MSDButtons.selectedSegmentIndex = send_byte / 16;
+    }
+}
+
+static const uint32_t BAUDS_REAL[] = {
+    9600, 19200, 38400, 57600, 76800, 115200
+};
+
+- (void)changedBaud:(id)sender
+{
+    uint32_t baud = BAUDS_REAL[self.baudButtons.selectedSegmentIndex];
+    [Konashi uartMode:KonashiUartModeDisable];
+    [Konashi uartBaudrate:(baud / 240)];
+    [Konashi uartMode:KonashiUartModeEnable];
+    [self pushLog:[NSString stringWithFormat:@"Konashi uartBaudrate %d", baud]];
+}
+
+- (void)clearRx:(id)sender
+{
+    self.uartRecvText.text = @"";
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,26 +90,37 @@
     NSLog(@"CONNECTED");
 }
 
+- (void) disconnected
+{
+    NSLog(@"DISCONNECTED");
+    [self pushLog:@"Konashi disconnected"];
+}
+
+- (IBAction)changedSendByte:(id)sender
+{
+    [self setSendData:(self.LSDButtons.selectedSegmentIndex + self.MSDButtons.selectedSegmentIndex * 16)];
+}
+
 - (void) ready
 {
     NSLog(@"READY");
     
     self.statusMessage.hidden = FALSE;
-    
-    [Konashi uartBaudrate:KonashiUartBaudrateRate9K6];
-    [Konashi uartMode:KonashiUartModeEnable];
-}
 
-- (void) recvUartRx
-{
-    NSLog(@"UartRx %d", [Konashi uartRead]);
+    [self pushLog:@"Konashi ready"];
+    [self changedBaud:0];
 }
 
 - (IBAction)find:(id)sender {
     [Konashi find];
 }
 
-- (IBAction)send:(id)sender {    
-    [Konashi uartWrite:'A'];
+- (IBAction)send:(id)sender {
+    @synchronized (self) {
+        if([Konashi isConnected]) {
+            [Konashi uartWriteData:[NSData dataWithBytes:&send_byte length:sizeof(send_byte)]];
+            [self pushLog:[NSString stringWithFormat:@"sent <%02X>", send_byte]];
+        }
+    }
 }
 @end
